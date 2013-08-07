3ef9bf051f2f51349a096496f528b0acfebfe098
Michele Simionato <michele@openquake.org>
2013-08-07 16:34:13 +0200

    Experimental version of the input tool for the engine

---
+++ input_tool/main.py
@@ -0,0 +1,36 @@
+import sys
+import nodes
+from PyQt4 import QtGui, QtCore
+from ui_input_tool_window import Ui_InputToolWindow
+
+
+class MainWindow(Ui_InputToolWindow, QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        self.setupUi(self)
+        print self.vSetsTbl
+        for vset in vsets:
+            print vset.
+
+# Main entry to program.  Set up the main app and create a new window.
+def main(argv):
+
+    # create Qt application
+    app = QtGui.QApplication(argv, True)
+
+    # create main window
+    wnd = MainWindow()
+    wnd.show()
+
+    # Start the app up
+    retval = app.exec_()
+    sys.exit(retval)
+
+
+root, modeltype = nodes.parse_nrml('/home/michele/oq-nrmllib/examples',
+                                   'vulnerability-model-discrete.xml')
+
+vsets = list(root)[1:]
+
+if __name__ == '__main__':
+    main(sys.argv)
