41b200dbaad4e5b9f0da18d38ee15141760c48f9
ptormene <paolo.tormene@globalquakemodel.org>
2013-10-24 15:49:59 +0200

    Added dialog to open input map and aggregation layer

--- svir/svirdialog.py
+++ svir/svirdialog.py
@@ -26,14 +26,55 @@
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from PyQt4 import QtCore, QtGui
 # create the dialog for zoom to point
+import os
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtGui import (QFileDialog,
+                         QDialog,
+                         QDialogButtonBox)
 from ui_svir import Ui_SvirDialog
 
 
-class SvirDialog(QtGui.QDialog):
+class SvirDialog(QDialog):
     def __init__(self):
-        QtGui.QDialog.__init__(self)
+        QDialog.__init__(self)
         # Set up the user interface from Designer.
         self.ui = Ui_SvirDialog()
         self.ui.setupUi(self)
+        self.ok_button = self.ui.buttonBox.button(QDialogButtonBox.Ok)
+        self.ok_button.setDisabled(True)
+
+    def open_file_dialog(self, dialog_type):
+        print 'here'
+        if dialog_type == 'loss_map':
+            text = self.tr('Select loss map')
+            filters = self.tr('Loss maps (*.shp);; All files (*.*)')
+        elif dialog_type == 'aggregation_layer':
+            text = self.tr('Select aggregation layer')
+            filters = self.tr('Aggregation layers (*.shp);; All files (*.*)')
+        else:
+            raise RuntimeError
+
+        return QFileDialog.getOpenFileName(self,
+                                               text,
+                                               os.path.expanduser('~'),
+                                               filters)
+
+    @pyqtSlot()
+    def on_input_layer_tbn_clicked(self):
+        file_loss_map = self.open_file_dialog('loss_map')
+        self.ui.input_layer_le.setText(file_loss_map)
+        self.enable_ok_button()
+
+    @pyqtSlot()
+    def on_aggregation_layer_tbn_clicked(self):
+        file_aggregation_layer = self.open_file_dialog('aggregation_layer')
+        self.ui.aggregation_layer_le.setText(file_aggregation_layer)
+        self.enable_ok_button()
+
+    def enable_ok_button(self):
+        if (os.path.isfile(self.ui.input_layer_le.text())
+                and os.path.isfile(self.ui.aggregation_layer_le.text())):
+            self.ok_button.setEnabled(True)
+        else:
+            self.ok_button.setEnabled(False)
--- svir/ui_svir.py
+++ svir/ui_svir.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_svir.ui'
 #
-# Created: Thu Oct 24 11:00:28 2013
+# Created: Thu Oct 24 14:43:04 2013
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,12 +17,35 @@ except AttributeError:
 class Ui_SvirDialog(object):
     def setupUi(self, SvirDialog):
         SvirDialog.setObjectName(_fromUtf8("SvirDialog"))
-        SvirDialog.resize(400, 300)
+        SvirDialog.resize(309, 113)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(SvirDialog)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.gridLayout = QtGui.QGridLayout()
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.input_layer_tbn = QtGui.QToolButton(SvirDialog)
+        self.input_layer_tbn.setObjectName(_fromUtf8("input_layer_tbn"))
+        self.gridLayout.addWidget(self.input_layer_tbn, 0, 2, 1, 1)
+        self.input_layer_lbl = QtGui.QLabel(SvirDialog)
+        self.input_layer_lbl.setObjectName(_fromUtf8("input_layer_lbl"))
+        self.gridLayout.addWidget(self.input_layer_lbl, 0, 0, 1, 1)
+        self.input_layer_le = QtGui.QLineEdit(SvirDialog)
+        self.input_layer_le.setObjectName(_fromUtf8("input_layer_le"))
+        self.gridLayout.addWidget(self.input_layer_le, 0, 1, 1, 1)
+        self.aggregation_layer_lbl = QtGui.QLabel(SvirDialog)
+        self.aggregation_layer_lbl.setObjectName(_fromUtf8("aggregation_layer_lbl"))
+        self.gridLayout.addWidget(self.aggregation_layer_lbl, 1, 0, 1, 1)
+        self.aggregation_layer_le = QtGui.QLineEdit(SvirDialog)
+        self.aggregation_layer_le.setObjectName(_fromUtf8("aggregation_layer_le"))
+        self.gridLayout.addWidget(self.aggregation_layer_le, 1, 1, 1, 1)
+        self.aggregation_layer_tbn = QtGui.QToolButton(SvirDialog)
+        self.aggregation_layer_tbn.setObjectName(_fromUtf8("aggregation_layer_tbn"))
+        self.gridLayout.addWidget(self.aggregation_layer_tbn, 1, 2, 1, 1)
+        self.verticalLayout_2.addLayout(self.gridLayout)
         self.buttonBox = QtGui.QDialogButtonBox(SvirDialog)
-        self.buttonBox.setGeometry(QtCore.QRect(30, 240, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.verticalLayout_2.addWidget(self.buttonBox)
 
         self.retranslateUi(SvirDialog)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), SvirDialog.accept)
@@ -31,4 +54,8 @@ class Ui_SvirDialog(object):
 
     def retranslateUi(self, SvirDialog):
         SvirDialog.setWindowTitle(QtGui.QApplication.translate("SvirDialog", "Svir", None, QtGui.QApplication.UnicodeUTF8))
+        self.input_layer_tbn.setText(QtGui.QApplication.translate("SvirDialog", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.input_layer_lbl.setText(QtGui.QApplication.translate("SvirDialog", "Input Layer", None, QtGui.QApplication.UnicodeUTF8))
+        self.aggregation_layer_lbl.setText(QtGui.QApplication.translate("SvirDialog", "Aggregation Layer", None, QtGui.QApplication.UnicodeUTF8))
+        self.aggregation_layer_tbn.setText(QtGui.QApplication.translate("SvirDialog", "...", None, QtGui.QApplication.UnicodeUTF8))
 
