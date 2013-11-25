908d62a418f1a72dd66738547961d9b062b6925d
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2013-11-25 18:02:18 +0100

    Much faster attribute updating; fixed icons

--- svir/layer_editing_manager.py
+++ svir/layer_editing_manager.py
@@ -6,12 +6,12 @@ class LayerEditingManager(object):
 
     def __enter__(self):
         self.layer.startEditing()
-        self.layer.beginEditCommand(self.message)
+        # self.layer.beginEditCommand(self.message)
         if self.debug:
             print "BEGIN", self.message
 
     def __exit__(self, type, value, traceback):
-        self.layer.endEditCommand()
+        # self.layer.endEditCommand()
         self.layer.commitChanges()
         self.layer.updateExtents()
         if self.debug:
