d27ff8dbcda53cdba9ee440d7167d65657e87e92
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2014-02-10 18:45:04 +0100

    Add docstrings to transformations

--- svir/normalization_algs.py
+++ svir/normalization_algs.py
@@ -46,42 +46,78 @@ def normalize(features_dict, algorithm, variant_name="", inverse=False):
 
 @NORMALIZATION_ALGS.add('RANK')
 def rank(input_list, variant_name="AVERAGE", inverse=False):
+    """Assign ranks to data, dealing with ties appropriately.
+
+    Args:
+        input_list: the list of numbers to rank
+        variant_name: available variants are
+            [AVERAGE, MIN, MAX, DENSE, ORDINAL] and they correspond to
+            different strategies on how to cope with ties (default: AVERAGE)
+        inverse: instead of giving the highest rank to the biggest input value,
+            give the highest rank to the smallest input value
+    Returns:
+        list of ranks corresponding to the input data
+    Raises:
+        NotImplementedError: if variant_name is not implemented
+    """
     input_copy = input_list[:]
     len_input_list = len(input_list)
-    rank_list = [0]*len_input_list
+    rank_list = [0] * len_input_list
     previous_ties = 0
-    if not inverse:
+    if not inverse:  # high values get high ranks
         # obtain a value above the maximum value contained in input_list,
         # so it will never be picked as the minimum element of the list
         above_max_input = max(input_list) + 1
         curr_idx = 1
         while curr_idx <= len_input_list:
-            # get the list of indices of the min elements of input_copy
+            # get the list of indices of the min elements of input_copy.
+            # note that we might have ties.
             bottom_indices = argwhere(
                 input_copy == amin(input_copy)).flatten().tolist()
             bottom_amount = len(bottom_indices)
             for bottom_idx in bottom_indices:
                 if variant_name == "AVERAGE":
+                    # e.g., if 4 inputs are equal, and they would receive
+                    # ranks from 3 to 6, all of them will obtain rank 4.5
+                    # Afterwards the ranks increase from 7 on.
                     rank_list[bottom_idx] = \
                         (2 * curr_idx + bottom_amount - 1) / 2.0
                 elif variant_name == "MIN":
+                    # e.g., if 4 inputs are equal, and they would receive
+                    # ranks from 3 to 6, all of them will obtain rank 3.
+                    # Afterwards the ranks increase from 7 on.
                     rank_list[bottom_idx] = curr_idx
                 elif variant_name == "MAX":
+                    # e.g., if 4 inputs are equal, and they would receive
+                    # ranks from 3 to 6, all of them will obtain rank 6
+                    # Afterwards the ranks increase from 7 on.
                     rank_list[bottom_idx] = curr_idx + bottom_amount - 1
                 elif variant_name == "DENSE":
+                    # the same as for "MIN", but instead of ranking the
+                    # next elements counting from 7 on, the ranks will
+                    # increase from 4 on (no "jumps").
                     rank_list[bottom_idx] = curr_idx - previous_ties
                 elif variant_name == "ORDINAL":
+                    # the ties are ranked in a "ordinal" way, assigning
+                    # the smallest rank to the leftmost tie found, and so on.
                     rank_list[bottom_idx] = curr_idx
                     curr_idx += 1
                 else:
                     raise NotImplementedError(
                         "%s variant not implemented" % variant_name)
+                # "discard" the tie, so it will not be found as the min in
+                # the next iteration (instead of removing it, which would
+                # modify the list indices, I make it bigger than the
+                # biggest input)
                 input_copy[bottom_idx] = above_max_input
             if variant_name != "ORDINAL":
+                # necessary adjustment in case of "ORDINAL"
                 curr_idx += bottom_amount
+            # necessary for the "DENSE" variant, to keep a count of ties
             previous_ties += bottom_amount - 1
 
-    else:  # inverse
+    else:  # inverse, i.e., small inputs get high ranks
+        # same as the direct methods, but proceeding top->down
         below_min_input = min(input_list) - 1
         curr_idx = len_input_list
         while curr_idx > 0:
@@ -115,7 +151,10 @@ def rank(input_list, variant_name="AVERAGE", inverse=False):
 @NORMALIZATION_ALGS.add('Z_SCORE')
 def z_score(input_list, variant_name=None, inverse=False):
     """
-    Normalized(e_i) = (e_i - mean(e)) / std(e)
+    Direct:
+        Normalized(e_i) = (e_i - mean(e)) / stddev(e)
+    Inverse:
+        Multiply each input by -1, before doing exactly the same
     """
     if variant_name:
         raise NotImplementedError("%s variant not implemented" % variant_name)
@@ -133,7 +172,11 @@ def z_score(input_list, variant_name=None, inverse=False):
 @NORMALIZATION_ALGS.add('MIN_MAX')
 def min_max(input_list, variant_name=None, inverse=False):
     """
-    Normalized(e_i) = (e_i - min(e)) / (max(e) - min(e))
+    Direct:
+        Normalized(e_i) = (e_i - min(e)) / (max(e) - min(e))
+    Inverse:
+        Normalized(e_i) = 1 - [(e_i - min(e)) / (max(e) - min(e))]
+
     """
     if variant_name:
         raise NotImplementedError("%s variant not implemented" % variant_name)
@@ -183,9 +226,12 @@ def log10_(input_list, variant_name=None, inverse=False):
 def simple_quadratic(input_list, variant_name="INCREASING", inverse=False):
     """
     Simple quadratic transformation (bottom = 0)
-    quadratic(e_i) = (e_i - bottom)^2 / (max(e) - bottom)^2
+        quadratic(e_i) = (e_i - bottom)^2 / (max(e) - bottom)^2
     ==>
-    simple_quadratic(e_i) = e_i^2 / max(e)^2
+        simple_quadratic(e_i) = e_i^2 / max(e)^2
+
+    Inverse:
+        For each output x, the final output will be 1 - x
     """
     bottom = 0.0
     max_input = max(input_list)
@@ -193,17 +239,17 @@ def simple_quadratic(input_list, variant_name="INCREASING", inverse=False):
         raise ZeroDivisionError("It is impossible to perform the "
                                 "transformation if the maximum "
                                 "input value is 0")
-    squared_range = (max_input - bottom)**2
+    squared_range = (max_input - bottom) ** 2
     if variant_name == "INCREASING":
         output_list = map(
-            lambda x: (x - bottom)**2 / squared_range, input_list)
+            lambda x: (x - bottom) ** 2 / squared_range, input_list)
     elif variant_name == "DECREASING":
         output_list = map(
-            lambda x: (max_input-(x-bottom))**2 / squared_range, input_list)
+            lambda x: (max_input - (x - bottom)) ** 2 / squared_range,
+            input_list)
 
     else:
         raise NotImplementedError("%s variant not implemented" % variant_name)
     if inverse:
         output_list[:] = [1.0 - x for x in output_list]
     return output_list
-
--- svir/normalization_dialog.py
+++ svir/normalization_dialog.py
@@ -25,7 +25,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 """
-from PyQt4 import QtCore
 from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import (QDialog,
                          QDialogButtonBox)
