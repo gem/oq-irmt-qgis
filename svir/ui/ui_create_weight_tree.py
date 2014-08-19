3d5f1417388cbc7a9974a5e8edeb997cc8e96a10
Marco Bernasocchi <marco@opengis.ch>
2014-08-19 15:42:57 +0200

    make ui

--- svir/ui/ui_create_weight_tree.py
+++ svir/ui/ui_create_weight_tree.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui/ui_create_weight_tree.ui'
 #
-# Created: Thu Jul  3 14:48:23 2014
-#      by: PyQt4 UI code generator 4.9.1
+# Created: Tue Aug 19 14:39:18 2014
+#      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,7 +12,16 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = lambda s: s
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_CreateWeightTreeDialog(object):
     def setupUi(self, CreateWeightTreeDialog):
@@ -42,7 +51,7 @@ class Ui_CreateWeightTreeDialog(object):
         sizePolicy.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
         self.label_4.setSizePolicy(sizePolicy)
         self.label_4.setObjectName(_fromUtf8("label_4"))
-        self.grid_layout.addWidget(self.label_4, 0, 2, 1, 1)
+        self.grid_layout.addWidget(self.label_4, 0, 2, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
         self.label_3 = QtGui.QLabel(self.scrollAreaWidgetContents)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -50,10 +59,10 @@ class Ui_CreateWeightTreeDialog(object):
         sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
         self.label_3.setSizePolicy(sizePolicy)
         self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.grid_layout.addWidget(self.label_3, 0, 1, 1, 1)
+        self.grid_layout.addWidget(self.label_3, 0, 1, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
         self.label_2 = QtGui.QLabel(self.scrollAreaWidgetContents)
         self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.grid_layout.addWidget(self.label_2, 0, 0, 1, 1)
+        self.grid_layout.addWidget(self.label_2, 0, 0, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
         self.gridLayout.addLayout(self.grid_layout, 0, 0, 1, 1)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.verticalLayout.addWidget(self.scrollArea)
@@ -69,9 +78,9 @@ class Ui_CreateWeightTreeDialog(object):
         QtCore.QMetaObject.connectSlotsByName(CreateWeightTreeDialog)
 
     def retranslateUi(self, CreateWeightTreeDialog):
-        CreateWeightTreeDialog.setWindowTitle(QtGui.QApplication.translate("CreateWeightTreeDialog", "Define model structure", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("CreateWeightTreeDialog", "Create your indices by defining which fields appartain to which theme. Attributes need both a theme and a name to be considered in the index calculation. You can generate a flat index (with no themes) by not entering any theme.", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_4.setText(QtGui.QApplication.translate("CreateWeightTreeDialog", "Name", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("CreateWeightTreeDialog", "Theme", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("CreateWeightTreeDialog", "Attribute", None, QtGui.QApplication.UnicodeUTF8))
+        CreateWeightTreeDialog.setWindowTitle(_translate("CreateWeightTreeDialog", "Define model structure", None))
+        self.label.setText(_translate("CreateWeightTreeDialog", "Create your indices by defining which fields appartain to which theme. Attributes need both a theme and a name to be considered in the index calculation. You can generate a flat index (with no themes) by not entering any theme.", None))
+        self.label_4.setText(_translate("CreateWeightTreeDialog", "Name", None))
+        self.label_3.setText(_translate("CreateWeightTreeDialog", "Theme", None))
+        self.label_2.setText(_translate("CreateWeightTreeDialog", "Attribute", None))
 
