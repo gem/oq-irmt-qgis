74f4fbd29dff34e75b4bda939ee99e4f927672b4
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2015-01-26 14:17:40 +0100

    Fix path for getting version from metadata

--- globals.py
+++ globals.py
@@ -23,6 +23,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 """
+import os
 from collections import OrderedDict
 from PyQt4.QtCore import QSettings
 from ConfigParser import ConfigParser
@@ -30,7 +31,7 @@ from ConfigParser import ConfigParser
 DEBUG = QSettings().value('/svir/developer_mode', False, type=bool)
 
 cp = ConfigParser()
-cp.readfp(open("metadata.txt"))
+cp.readfp(open(os.path.dirname(os.path.realpath(__file__)) + "/metadata.txt"))
 SVIR_PLUGIN_VERSION = cp.get('general', 'version')
 
 INT_FIELD_TYPE_NAME = "integer"
