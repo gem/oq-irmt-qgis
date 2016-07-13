f0ad922c52bd7355a83e1bbebb18335f12965957
Marco Bernasocchi <marco@opengis.ch>
2016-07-13 19:55:17 +0200

    added export buttons and IMT selector

--- svir/dialogs/viewer_dock.py
+++ svir/dialogs/viewer_dock.py
@@ -23,18 +23,23 @@
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
+import numpy as np
+import os
 
 from PyQt4 import QtGui
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import (
     FigureCanvasQTAgg as FigureCanvas,
-    #NavigationToolbar2QT as NavigationToolbar
+    # NavigationToolbar2QT as NavigationToolbar
 )
 
 from qgis.core import QgsFeatureRequest
 
-from svir.utilities.utils import get_ui_class
+from PyQt4.QtCore import pyqtSlot
+
+from svir.utilities.shared import TEXTUAL_FIELD_TYPES
+from svir.utilities.utils import get_ui_class, reload_attrib_cbx
 
 FORM_CLASS = get_ui_class('ui_viewer_dock.ui')
 
@@ -57,22 +62,35 @@ class ViewerDock(QtGui.QDockWidget, FORM_CLASS):
         self.active_layer = self.iface.activeLayer()
 
         self.current_selection = {}
+        self.current_imt = None
 
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
-        self.main_layout.addWidget(self.canvas)
+        self.central_layout.addWidget(self.canvas)
         self.plot = self.figure.add_subplot(111)
 
     def draw(self):
         self.plot.clear()
         for index, line in self.current_selection.iteritems():
-            # print (index, line)
-            self.plot.plot(line['ordinates'])
+
+            ordinates = line['ordinates']
+            # FIXME use abscissa values from hdf5 file
+            abscissa = np.linspace(0.0, 1.0, num=len(ordinates))
+
+            self.plot.plot(
+                    abscissa,
+                    ordinates,
+                    color=line['color'],
+                    linestyle='solid',
+                    label='site ' + str(index)
+            )
+            self.plot.legend()
 
         self.canvas.draw()
 
     def layer_changed(self):
         self.current_selection = {}
+        self.plot.clear()
 
         try:
             self.active_layer.selectionChanged.disconnect(
@@ -85,12 +103,17 @@ class ViewerDock(QtGui.QDockWidget, FORM_CLASS):
         if self.active_layer is not None:
             self.active_layer.selectionChanged.connect(self.selection_changed)
 
+            reload_attrib_cbx(
+                    self.imt_cbx, self.active_layer, False, TEXTUAL_FIELD_TYPES)
+
+            if self.active_layer.selectedFeatureCount() > 0:
+                self.set_selection(self.active_layer.selectedFeaturesIds())
+
+
+    def set_selection(self, selected):
+        self.selection_changed(selected, [], None)
+
     def selection_changed(self, selected, deselected, _):
-        arg = 'PGA'
-
-        print ('Selected: ', selected)
-        print ('Deselected: ', deselected)
-
         selected_features = self.active_layer.getFeatures(
                 QgsFeatureRequest().setFilterFids(selected))
 
@@ -101,7 +124,7 @@ class ViewerDock(QtGui.QDockWidget, FORM_CLASS):
                 pass
 
         for feature in selected_features:
-            ordinates = json.loads(feature[arg])
+            ordinates = json.loads(feature[self.current_imt])
             if feature.id() not in self.current_selection:
                 self.current_selection[feature.id()] = {
                     'ordinates': ordinates,
@@ -110,6 +133,30 @@ class ViewerDock(QtGui.QDockWidget, FORM_CLASS):
 
         self.draw()
 
+    @pyqtSlot(int)
+    def on_imt_cbx_currentIndexChanged(self):
+        self.current_imt = self.imt_cbx.currentText()
+        self.set_selection(self.current_selection.keys())
 
+    @pyqtSlot()
+    def on_export_data_button_clicked(self):
 
+        filename = QtGui.QFileDialog.getSaveFileName(
+                self,
+                self.tr('Export data'),
+                os.path.expanduser('~/hazard_curves_%s.csv' % self.current_imt),
+                '*.csv')
 
+        if filename:
+            print filename
+
+    @pyqtSlot()
+    def on_export_image_button_clicked(self):
+        filename = QtGui.QFileDialog.getSaveFileName(
+                self,
+                self.tr('Export plot'),
+                os.path.expanduser('~/hazard_curves_%s.png' % self.current_imt),
+                '*.png')
+
+        if filename:
+            print filename
