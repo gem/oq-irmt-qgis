083ff0b4de1dd808c76d41996f0339e0eb280df9
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2016-07-22 13:50:33 +0200

    Remove generated ui_*.py files

--- svir/ui/ui_drive_engine_server.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui/ui_drive_engine_server.ui'
-#
-# Created: Thu Jul 21 16:51:54 2016
-#      by: PyQt4 UI code generator 4.10.4
-#
-# WARNING! All changes made in this file will be lost!
-
--- svir/ui/ui_transformation.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui/ui_transformation.ui'
-#
-# Created: Fri Jul 22 09:45:27 2016
-#      by: PyQt4 UI code generator 4.10.4
-#
-# WARNING! All changes made in this file will be lost!
-
