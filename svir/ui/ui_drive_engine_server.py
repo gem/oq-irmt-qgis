f92f9446ffdd108724c9517426e25b4f32c1725b
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2016-07-25 10:43:40 +0200

    Remove Ok and Cancel buttons from the non-modal window driving the oq-engine

--- svir/ui/ui_drive_engine_server.py
+++ svir/ui/ui_drive_engine_server.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/ui_drive_engine_server.ui'
 #
-# Created: Thu Jul 21 16:51:54 2016
+# Created: Mon Jul 25 09:49:47 2016
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
