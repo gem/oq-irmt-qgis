add6744f6b100a90fb4d97dd8180f61e13cc5270
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2016-07-25 10:42:38 +0200

    Remove registration link in case of engine settings

--- svir/dialogs/settings_dialog.py
+++ svir/dialogs/settings_dialog.py
@@ -52,7 +52,7 @@ class SettingsDialog(QtGui.QDialog, FORM_CLASS):
         elif self.server == 'engine':
             self.topGroupBox.setTitle(
                 'OpenQuake Engine connection settings')
-            self.registration_link_lbl.setText('FIXME Link')
+            self.registration_link_lbl.hide()
         else:
             raise ValueError(self.server)
         self.restoreState()
