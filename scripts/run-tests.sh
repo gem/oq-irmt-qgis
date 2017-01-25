94bd8c272201a268b0090cfbafc37570518e1cb5
Daniele Viganò <daniele@vigano.me>
2017-01-25 10:21:44 +0100

    Change test script for CI

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,16 +3,27 @@
 #display each command before executing it
 set -x
 
-UBUNTU_VERSION="xenial"
-QGIS_VERSION="debian-ltr"
-IRMT_BRANCH='recovery-play'
+SOURCE="${BASH_SOURCE[0]}"
+THISDIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+if [ -z $UBUNTU_VERSION ]; then
+    UBUNTU_VERSION="xenial"
+fi
+if [ -z $QGIS_VERSION ]; then
+    QGIS_VERSION="debian-ltr"
+fi
+if [ -z $IRMT_BRANCH ]; then
+    IRMT_BRANCH="master"
+fi
 
 export DISPLAY=:1
+
 echo "deb http://qgis.org/$QGIS_VERSION $UBUNTU_VERSION main" | sudo tee /etc/apt/sources.list.d/qgis.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 073D307A618E5811
 sudo apt update
 sudo apt install -y qgis python-mock python-nose python-coverage python-scipy
-git clone --depth=1 -b $IRMT_BRANCH https://github.com/gem/oq-irmt-qgis.git
-cd oq-irmt-qgis/svir
-source ../scripts/run-env-linux.sh /usr
+
+source $THISDIR/run-env-linux.sh /usr
+
+cd $THISDIR/../svir
 make test
