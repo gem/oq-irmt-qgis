33c13a01654b98addf45c8712545092ac345f7c3
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2017-01-25 09:35:56 +0100

    Minor change to testing script

--- scripts/build-testing-machine.sh
+++ scripts/build-testing-machine.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
+#display each command before executing it
+set -x
+
 UBUNTU_VERSION="xenial"
 QGIS_VERSION="debian-ltr"
-GEM_SET_BRANCH='recovery-play'
-startlxde
+IRMT_BRANCH='recovery-play'
+
 export DISPLAY=:1
 echo "deb http://qgis.org/$QGIS_VERSION $UBUNTU_VERSION main" | sudo tee /etc/apt/sources.list.d/qgis.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 073D307A618E5811
 sudo apt update
 sudo apt install -y qgis python-mock python-nose python-coverage python-scipy
-git clone --depth=1 -b $GEM_SET_BRANCH https://github.com/gem/oq-irmt-qgis.git
+git clone --depth=1 -b $IRMT_BRANCH https://github.com/gem/oq-irmt-qgis.git
 cd oq-irmt-qgis/svir
 source ../scripts/run-env-linux.sh /usr
 make test
