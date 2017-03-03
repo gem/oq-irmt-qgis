4eed4c326e8025097f9929723839d915f9c4f0d0
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2017-03-03 10:21:18 +0100

    Let the plot in the data viewer occupy all the available space

--- svir/dialogs/viewer_dock.py
+++ svir/dialogs/viewer_dock.py
@@ -125,6 +125,7 @@ class ViewerDock(QtGui.QDockWidget, FORM_CLASS):
 
         self.plot_figure = Figure()
         self.plot_canvas = FigureCanvas(self.plot_figure)
+        self.plot_canvas.setSizePolicy(size_policy, size_policy)
         self.plot_toolbar = NavigationToolbar(self.plot_canvas, self)
         self.plot = self.plot_figure.add_subplot(111)
         self.legend = None
