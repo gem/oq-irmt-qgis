c91c921af830720227b914cf68383a4d69d7cf51
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2017-04-13 17:42:14 +0200

    Add tests loading losses_by_asset

--- svir/test/test_load_oq_engine_output_as_layer.py
+++ svir/test/test_load_oq_engine_output_as_layer.py
@@ -42,6 +42,8 @@ from svir.dialogs.load_gmf_data_as_layer_dialog import (
     LoadGmfDataAsLayerDialog)
 from svir.dialogs.load_uhs_as_layer_dialog import (
     LoadUhsAsLayerDialog)
+from svir.dialogs.load_losses_by_asset_as_layer_dialog import (
+    LoadLossesByAssetAsLayerDialog)
 from svir.dialogs.viewer_dock import ViewerDock
 from svir.calculations.process_layer import ProcessLayer
 from utilities import get_qgis_app
@@ -128,7 +130,7 @@ class LoadOQEngineOutputAsLayerTestCase(unittest.TestCase):
         # test exporting the current selection to csv
         self._test_export('uniform_hazard_spectra.csv')
 
-    def test_dmg_by_asset(self):
+    def test_load_dmg_by_asset(self):
         filepath = os.path.join(
             self.data_dir_name, 'risk',
             'output-308-dmg_by_asset-ChiouYoungs2008()_103.csv')
@@ -162,6 +164,37 @@ class LoadOQEngineOutputAsLayerTestCase(unittest.TestCase):
         reference_layer = QgsVectorLayer(
             reference_path, 'reference_ruptures', 'ogr')
         ProcessLayer(current_layer).has_same_content_as(reference_layer)
+
+    def test_load_losses_by_asset_only_selected_taxonomy_and_loss_type(self):
+        filepath = os.path.join(self.data_dir_name, 'risk',
+                                'output-399-losses_by_asset_123.npz')
+        dlg = LoadLossesByAssetAsLayerDialog(
+            IFACE, 'losses_by_asset', filepath)
+        dlg.load_selected_only_ckb.setChecked(True)
+        taxonomy_idx = dlg.taxonomy_cbx.findText('"Concrete"')
+        self.assertNotEqual(taxonomy_idx, -1,
+                            'Taxonomy "Concrete" was not found')
+        dlg.taxonomy_cbx.setCurrentIndex(taxonomy_idx)
+        loss_type_idx = dlg.loss_type_cbx.findText('structural')
+        self.assertNotEqual(loss_type_idx, -1,
+                            'Loss type structural was not found')
+        dlg.loss_type_cbx.setCurrentIndex(loss_type_idx)
+        dlg.accept()
+
+    def test_load_losses_by_asset_all_taxonomies_only_selected_loss_type(self):
+        filepath = os.path.join(self.data_dir_name, 'risk',
+                                'output-399-losses_by_asset_123.npz')
+        dlg = LoadLossesByAssetAsLayerDialog(
+            IFACE, 'losses_by_asset', filepath)
+        dlg.load_selected_only_ckb.setChecked(True)
+        taxonomy_idx = dlg.taxonomy_cbx.findText('All')
+        self.assertNotEqual(taxonomy_idx, -1, 'Taxonomy All was not found')
+        dlg.taxonomy_cbx.setCurrentIndex(taxonomy_idx)
+        loss_type_idx = dlg.loss_type_cbx.findText('structural')
+        self.assertNotEqual(loss_type_idx, -1,
+                            'Loss type structural was not found')
+        dlg.loss_type_cbx.setCurrentIndex(loss_type_idx)
+        dlg.accept()
 
     def _test_export(self, expected_file_name):
         _, exported_file_path = tempfile.mkstemp(suffix=".csv")
