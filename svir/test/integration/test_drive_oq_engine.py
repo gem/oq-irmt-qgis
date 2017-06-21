7c630d61512c0d7c2095362ac3b68fb16611ad82
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2017-06-21 17:24:37 +0200

    Remove duplicated code

--- svir/test/integration/test_drive_oq_engine.py
+++ svir/test/integration/test_drive_oq_engine.py
@@ -182,9 +182,6 @@ class LoadOqEngineOutputsTestCase(unittest.TestCase):
         else:
             print('\n\tSELECTED_OTYPE is set.'
                   ' Running tests only for %s' % self.selected_otype)
-        if selected_calc_id is not None:
-            calc_list = [calc for calc in calc_list
-                         if calc['id'] == selected_calc_id]
         for calc in calc_list:
             print('\nCalculation %s: %s' % (calc['id'], calc['description']))
             self.load_calc_outputs(calc)
