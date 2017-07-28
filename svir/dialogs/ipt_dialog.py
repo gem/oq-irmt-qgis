c03455f64792df40415bf5c136380923f862c0d1
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2017-07-28 15:15:05 +0200

    Example of handling links from QWebView

--- svir/dialogs/ipt_dialog.py
+++ svir/dialogs/ipt_dialog.py
@@ -22,8 +22,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt4.QtCore import QUrl
+from qgis.PyQt.QtWebKit import QWebPage
+from qgis.PyQt.QtCore import QUrl
 from qgis.PyQt.QtGui import QDialog, QDialogButtonBox
+from third_party import requests
 from svir.utilities.utils import get_ui_class
 
 FORM_CLASS = get_ui_class('ui_ipt.ui')
@@ -37,5 +39,44 @@ class IptDialog(QDialog, FORM_CLASS):
         # Set up the user interface from Designer.
         self.setupUi(self)
         self.ok_button = self.buttonBox.button(QDialogButtonBox.Ok)
+        qurl = QUrl(
+            # 'https://platform.openquake.org/ipt')
+            # FIXME: loading a page that offers a link to download a small txt
+            'http://www.sample-videos.com/download-sample-text-file.php')
+        self.web_view.setUrl(qurl)
 
-        self.web_view.setUrl(QUrl('https://platform.openquake.org/ipt/'))
+        # downloadRequested(QNetworkRequest) is a signal that is triggered in
+        # the web page when the user right-clicks on a link and chooses "save
+        # link as". Instead of proceeding with the normal workflow (asking the
+        # user where to save the file) the control is passed to self.download,
+        # that retrieves the file and prints its contents
+        self.web_view.page().downloadRequested.connect(
+            self.handle_downloadRequested)
+
+        # NOTE: without the following line, linkClicked is not emitted, but
+        # we would need to delegate only one specific link!
+        self.web_view.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
+        self.web_view.page().linkClicked.connect(self.handle_linkClicked)
+
+        self.web_view.page().linkHovered.connect(self.handle_linkHovered)
+
+    def handle_downloadRequested(self, request):
+        print('Downloaded file:')
+        resp = requests.get(request.url().toString())
+        print(resp.content)
+
+    def handle_linkClicked(self, url):
+        print('Downloaded file:')
+        resp = requests.get(url.toString())
+        print(resp.content)
+
+    def handle_linkHovered(self, link, title, text_content):
+        print(link)
+
+    def on_set_example_btn_clicked(self):
+        qurl = QUrl(
+            'https://platform.openquake.org/ipt/?tab_id=1&example_id=99')
+        self.web_view.setUrl(qurl)
+
+    def on_back_btn_clicked(self):
+        self.web_view.back()
