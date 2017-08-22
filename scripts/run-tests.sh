7d6d5e203a5cabbb3921eca23468db8aeac19c76
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2017-08-22 10:04:14 +0200

    Attempt to fix unit and integration tests on Jenkins

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -20,10 +20,24 @@ fi
 
 export DISPLAY=:1
 
+if [ "$(git ls-remote --heads https://github.com/gem/oq-engine.git ${IRMT_BRANCH})" == "" ]; then
+  BRANCH='master';
+fi;
+curl -sfO http://artifacts.openquake.org/travis/oqdata-${BRANCH}.zip || ( echo "Dump for ${BRANCH} unavailable"; exit 1 )
+git clone -q -b ${BRANCH} --depth=1 https://github.com/gem/oq-engine.git && echo "Running against oq-engine/${BRANCH}"
 echo "deb http://qgis.org/$QGIS_VERSION $UBUNTU_VERSION main" | sudo tee /etc/apt/sources.list.d/qgis.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key CAEB3DC3BDF7FB45
-sudo apt update
-sudo apt install -y qgis python-mock python-nose python-nose-exclude python-scipy
+sudo add-apt-repository -y ppa:openquake/saga
+sudo apt-get update -q
+sudo apt install -y qgis python-mock python-nose python-nose-exclude python-scipy saga python-saga
+
+virtualenv oqe27
+oqe27/bin/pip -q install -U pip
+oqe27/bin/pip -q install -r oq-engine/requirements-py27-linux64.txt
+oqe27/bin/pip -q install -e oq-engine
+
+oqe27/bin/oq restore oqdata-${BRANCH}.zip ~/oqdata
+oqe27/bin/oq webui start --skip-browser &> webui.log &
 
 source $REPODIR/scripts/run-env-linux.sh /usr
 
