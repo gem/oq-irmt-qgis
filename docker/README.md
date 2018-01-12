dd80e3963e25f2fa79cd90c181b5c294ab6fbe14
Daniele Viganò <daniele@vigano.me>
2018-01-12 15:01:44 +0100

    Several enhancemets to Docker image

--- docker/README.md
+++ docker/README.md
@@ -14,5 +14,5 @@ On our Jenkins system `--build-arg` must be `--build-arg uid=107`
 From the root folder:
 
 ```bash
-$ docker run -t -i -v $(pwd):/io --rm qgis-builder
+$ docker run -t -i -v $(pwd):/io --rm qgis-builder package VERSION=X.YY
 ```
--- docker/run_make.sh
+++ docker/run_make.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    echo "ERROR: missing argument" >&2
+    exit 1
+fi
+
 # Start Xvfb on :99
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16 2>&1 >/dev/null &
 cd svir && source ../scripts/run-env-linux.sh /usr
-make deploy
+make $*
