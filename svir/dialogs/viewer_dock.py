d75aad944d2c0fd564bb122ffa84ecc268bae6f0
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2018-04-04 15:26:53 +0200

    Set stats to ['rlz-0'] if stats equal to numpy.array(None)

--- svir/dialogs/viewer_dock.py
+++ svir/dialogs/viewer_dock.py
@@ -660,10 +660,10 @@ class ViewerDock(QDockWidget, FORM_CLASS):
         to_extract = 'agglosses/%s' % loss_types[0]
         npz = extract_npz(session, hostname, calc_id, to_extract,
                           message_bar=self.iface.messageBar())
-        self.stats = str(npz['stats']).split()
-        if not self.stats:
+        if npz['stats'] == numpy.array(None):
             self.stats = ['rlz-0']
-
+        else:
+            self.stats = str(npz['stats']).split()
         self.tag_names_multiselect.set_unselected_items(self.tags.keys())
         self.tag_names_multiselect.set_selected_items([])
         self.tag_values_multiselect.set_unselected_items([])
