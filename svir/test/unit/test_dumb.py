5eee235af2bc145af5a9d476054da12f8cb095e2
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2018-05-18 14:46:22 +0200

    Add a dumb test, to investigate segfault [skip CI]

---
+++ svir/test/unit/test_dumb.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#/***************************************************************************
+# Irmt
+#                                 A QGIS plugin
+# OpenQuake Integrated Risk Modelling Toolkit
+#                              -------------------
+#        begin                : 2013-10-24
+#        copyright            : (C) 2013 by GEM Foundation
+#        email                : devops@openquake.org
+# ***************************************************************************/
+#
+# Copyright (c) 2013-2014, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+# from svir.test.utilities import get_qgis_app
+# QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
+
+
+class DumbTest(unittest.TestCase):
+    """Test nothing."""
+
+    def setUp(self):
+        """Runs before each test."""
+        pass
+
+    def tearDown(self):
+        """Runs after each test."""
+        pass
+
+    def test_dumb(self):
+        pass
