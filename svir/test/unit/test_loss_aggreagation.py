084d5d6dcf5d681be8c3d9ff1c3a28691c0b774c
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2018-06-05 14:21:35 +0200

    Use temp copies of gpkg files in tests

--- svir/test/unit/test_loss_aggreagation.py
+++ svir/test/unit/test_loss_aggreagation.py
@@ -26,6 +26,7 @@
 import os.path
 import unittest
 import tempfile
+import shutil
 from qgis.core import QgsVectorLayer
 from svir.calculations.process_layer import ProcessLayer
 from svir.calculations.aggregate_loss_by_zone import calculate_zonal_stats
@@ -44,14 +45,23 @@ class AggregateLossByZoneTestCase(unittest.TestCase):
         self.loss_layer_is_vector = True
 
     def test_sum_point_values_by_zone(self):
+        # NOTE: it shouldn't be necessary to make copies of gpkg files, because
+        # they are not modified by the test. However, when QGIS opens a gpkg
+        # (and it's not protected from writing), it modifies the file even if
+        # no changes are made and if it's not saved afterwards. It seems like a
+        # bug in QGIS.
         points_layer_path = os.path.join(
             self.data_dir_name, 'loss_points.gpkg')
+        points_copy_path = tempfile.NamedTemporaryFile(suffix='.gpkg').name
+        shutil.copyfile(points_layer_path, points_copy_path)
         points_layer = QgsVectorLayer(
-            points_layer_path, 'Loss points having zone ids', 'ogr')
+            points_copy_path, 'Loss points having zone ids', 'ogr')
         zonal_layer_path = os.path.join(
             self.data_dir_name, 'svi_zones.gpkg')
+        zonal_copy_path = tempfile.NamedTemporaryFile(suffix='.gpkg').name
+        shutil.copyfile(zonal_layer_path, zonal_copy_path)
         zonal_layer = QgsVectorLayer(
-            zonal_layer_path, 'SVI zones', 'ogr')
+            zonal_copy_path, 'SVI zones', 'ogr')
         output_zonal_layer = calculate_zonal_stats(
             zonal_layer, points_layer, self.loss_attr_names,
             'output', discard_nonmatching=False,
@@ -59,9 +69,14 @@ class AggregateLossByZoneTestCase(unittest.TestCase):
         expected_zonal_layer_path = os.path.join(
             self.data_dir_name,
             'svi_zones_plus_loss_stats.gpkg')
+        expected_copy_path = tempfile.NamedTemporaryFile(suffix='.gpkg').name
+        shutil.copyfile(expected_zonal_layer_path, expected_copy_path)
         expected_zonal_layer = QgsVectorLayer(
-            expected_zonal_layer_path, 'Expected zonal layer', 'ogr')
+            expected_copy_path, 'Expected zonal layer', 'ogr')
         self._check_output_layer(output_zonal_layer, expected_zonal_layer)
+        os.remove(points_copy_path)
+        os.remove(zonal_copy_path)
+        os.remove(expected_copy_path)
 
     def _check_output_layer(self, output_layer, expected_layer):
         if not ProcessLayer(output_layer).has_same_content_as(
