179eb85222139dbde0300dc5d1df2d1b050c2c49
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2018-07-16 16:55:50 +0200

    Fix retrieval of loss_type

--- svir/dialogs/viewer_dock.py
+++ svir/dialogs/viewer_dock.py
@@ -1384,12 +1384,13 @@ class ViewerDock(QDockWidget, FORM_CLASS):
                     '~/uniform_hazard_spectra_%s.csv' % self.calc_id),
                 '*.csv')
         elif self.output_type in ['agg_curves-rlzs', 'agg_curves-stats']:
+            loss_type = self.loss_type_cbx.currentText()
             filename, _ = QFileDialog.getSaveFileName(
                 self,
                 self.tr('Export data'),
                 os.path.expanduser(
                     '~/%s_%s_%s.csv' % (self.output_type,
-                                        self.current_loss_type,
+                                        loss_type,
                                         self.calc_id)),
                 '*.csv')
         elif self.output_type in ('dmg_by_asset_aggr',
@@ -1480,9 +1481,10 @@ class ViewerDock(QDockWidget, FORM_CLASS):
                 headers = ['return_period']
                 headers.extend(['rlz-%s' % rlz for rlz in range(num_rlzs)])
                 writer.writerow(headers)
+                loss_type = self.loss_type_cbx.currentText()
                 for i, return_period in enumerate(
                         self.agg_curves['return_periods']):
-                    values = self.agg_curves['array'][self.current_loss_type]
+                    values = self.agg_curves['array'][loss_type]
                     row = [return_period]
                     row.extend([value for value in values[i]])
                     writer.writerow(row)
