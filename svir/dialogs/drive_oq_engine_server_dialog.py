0617654c2cbffcf44991d8254d6d70eed46d34ad
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2018-07-19 15:40:16 +0200

    Disable loading dmg/losses_by_event as layer for event_based calcs

--- svir/dialogs/drive_oq_engine_server_dialog.py
+++ svir/dialogs/drive_oq_engine_server_dialog.py
@@ -661,9 +661,11 @@ class DriveOqEngineServerDialog(QDialog, FORM_CLASS):
             if row['type'] in (OQ_TO_LAYER_TYPES |
                                OQ_RST_TYPES |
                                OQ_EXTRACT_TO_VIEW_TYPES):
-                # TODO: remove check when gmf_data will be loadable also for
-                #       event_based
-                if not (row['type'] == 'gmf_data'
+                # TODO: remove check when gmf_data, dmg_by_event and
+                #       losses_by_event will be loadable also for event_based
+                if not (row['type'] in ['gmf_data',
+                                        'dmg_by_event',
+                                        'losses_by_event']
                         and 'event_based' in calculation_mode):
                     num_actions += 1  # needs additional column for loader btn
             if "%s_aggr" % row['type'] in OQ_EXTRACT_TO_VIEW_TYPES:
@@ -696,7 +698,11 @@ class DriveOqEngineServerDialog(QDialog, FORM_CLASS):
                         action = 'Load as layer'
                     # TODO: remove check when gmf_data will be loadable also
                     #       for event_based
-                    if (output['type'] == 'gmf_data'
+                    # TODO: remove check when dmg_by_event and losses_by_event
+                    #       will be loadable also for event_based
+                    if (output['type'] in ['gmf_data',
+                                           'dmg_by_event',
+                                           'losses_by_event']
                             and calculation_mode == 'event_based'):
                         continue
                     button = QPushButton()
