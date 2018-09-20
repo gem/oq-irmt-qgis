c541929c505ebbc916bd3a19c9beada37fc82c5f
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2018-09-20 15:50:52 +0200

    Remove all references to third_party packages

--- svir/utilities/utils.py
+++ svir/utilities/utils.py
@@ -54,7 +54,6 @@ from qgis.PyQt.QtWidgets import (
                                  )
 from qgis.PyQt.QtGui import QColor
 
-from svir.third_party.poster.encode import multipart_encode
 from svir.utilities.shared import (
                                    DEBUG,
                                    DEFAULT_SETTINGS,
@@ -674,32 +673,6 @@ class ReadMetadataError(Exception):
     """When a metadata xml is not correctly formatted can't be read"""
     suggestion = (
         'Check that the file is correct')
-
-
-class IterableToFileAdapter(object):
-    """ an adapter which makes the multipart-generator issued by poster
-        accessible to requests. Based upon code from
-        http://stackoverflow.com/a/13911048/1659732
-        https://goo.gl/zgLx0T
-    """
-    def __init__(self, iterable):
-        self.iterator = iter(iterable)
-        self.length = iterable.total
-
-    def read(self, size=-1):
-        return next(self.iterator, b'')
-
-    def __len__(self):
-        return self.length
-
-
-def multipart_encode_for_requests(params, boundary=None, cb=None):
-    """"helper function simulating the interface of posters
-        multipart_encode()-function
-        but wrapping its generator with the file-like adapter
-    """
-    data_generator, headers = multipart_encode(params, boundary, cb)
-    return IterableToFileAdapter(data_generator), headers
 
 
 def write_layer_suppl_info_to_qgs(layer_id, suppl_info):
