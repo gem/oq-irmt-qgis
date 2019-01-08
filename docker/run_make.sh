206b8c69f96036adda45975dc5685908fbdd1c9a
Daniele Viganò <daniele@vigano.me>
2019-01-08 11:47:44 +0100

    Quick fix for run_make.sh

--- docker/run_make.sh
+++ docker/run_make.sh
@@ -13,7 +13,7 @@ sudo apt upgrade -y
 if [ -f .osgeo_credentials ]; then . .osgeo_credentials; fi
 
 # Make sure XDG home structure exists
-mkdir -p ${HOME}/.local/share/
+mkdir -p ${HOME}/.local/share/QGIS
 
 # Start Xvfb on :99
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16 2>&1 >/dev/null &
