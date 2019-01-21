4ac432877e141d70904e2cbe3e5a145ca5f6b1ba
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-01-21 16:26:56 +0100

    Run unit tests in verbose mode

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -9,4 +9,4 @@ docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
 
 docker exec -it qgis sh -c "apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit python3-nose"
 
-docker exec -it qgis sh -c "export PYTHONPATH=/usr/share/qgis/python/plugins/:$PYTHONPATH; nosetests3 /tests_directory/svir/test/unit/"
+docker exec -it qgis sh -c "export PYTHONPATH=/usr/share/qgis/python/plugins/:$PYTHONPATH; nosetests3 -v /tests_directory/svir/test/unit/"
