b8f6856ddf3884cadf3d11da19ebea1c98ccfc68
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-04-01 10:40:44 +0200

    Fix accept dialog

--- svir/dialogs/load_assets_as_layer_dialog.py
+++ svir/dialogs/load_assets_as_layer_dialog.py
@@ -22,6 +22,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
+from qgis.PyQt.QtWidgets import QDialog
 from qgis.core import (
     QgsFeature, QgsGeometry, QgsPointXY, edit, QgsTask, QgsApplication,
     QgsProject,)
@@ -118,6 +119,7 @@ class LoadAssetsAsLayerDialog(LoadOutputAsLayerDialog):
         self.hide()
         extract_params = self.get_extract_params()
         self.download_assets(extract_params)
+        QDialog.accept(self)
 
     def get_extract_params(self):
         params = {}
