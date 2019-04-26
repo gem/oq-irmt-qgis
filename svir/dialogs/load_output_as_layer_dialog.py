dd8d58aaba192853e8d067ea9ff60260c3deb491
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-04-26 10:50:19 +0200

    Failed attempt to set incremental symbol levels

--- svir/dialogs/load_output_as_layer_dialog.py
+++ svir/dialogs/load_output_as_layer_dialog.py
@@ -681,6 +681,12 @@ class LoadOutputAsLayerDialog(QDialog, FORM_CLASS):
                 rule.setLabel(simplified)
             root_rule.removeChildAt(0)
             renderer = rule_renderer
+        renderer.setUsingSymbolLevels(True)
+        symbol_items = [item for item in renderer.legendSymbolItems()]
+        for i in range(len(symbol_items)):
+            sym = symbol_items[i].symbol()
+            for lay in range(sym.symbolLayerCount()):
+                sym.symbolLayer(lay).setRenderingPass(i)
         layer.setRenderer(renderer)
         layer.setOpacity(0.7)
         layer.triggerRepaint()
