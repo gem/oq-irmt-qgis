c4701975cb4687cbb2a8178a76eadae077d9b96e
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-05-02 09:44:08 +0200

    Remove dead code [skip CI]

--- svir/dialogs/load_inputs_dialog.py
+++ svir/dialogs/load_inputs_dialog.py
@@ -22,8 +22,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-# import os
-# import tempfile
 import zipfile
 import json
 import os
