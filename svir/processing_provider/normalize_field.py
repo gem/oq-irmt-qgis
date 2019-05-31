eb1154ac24d30517347b4bea7475219831e5d85f
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-05-31 17:09:37 +0200

    Inherit from a common TransformFieldAlgorithm class

--- svir/processing_provider/normalize_field.py
+++ svir/processing_provider/normalize_field.py
@@ -1,55 +1,42 @@
 # -*- coding: utf-8 -*-
+# /***************************************************************************
+# Irmt
+#                                 A QGIS plugin
+# OpenQuake Integrated Risk Modelling Toolkit
+#                              -------------------
+#        begin                : 2013-10-24
+#        copyright            : (C) 2013-2019 by GEM Foundation
+#        email                : devops@openquake.org
+# ***************************************************************************/
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
-"""
-
-from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtCore import QCoreApplication
-from qgis.core import (QgsProcessing,
+from qgis.core import (
                        QgsFeatureSink,
                        QgsFeature,
                        QgsField,
                        QgsProcessingException,
-                       QgsProcessingAlgorithm,
-                       QgsProcessingUtils,
-                       QgsProcessingParameterFeatureSource,
-                       QgsProcessingParameterField,
-                       QgsProcessingParameterBoolean,
-                       QgsProcessingParameterFeatureSink)
+                       )
 from processing.tools import vector
+from svir.processing_provider.transform_field import TransformFieldAlgorithm
 
 
-class NormalizeFieldAlgorithm(QgsProcessingAlgorithm):
+class NormalizeFieldAlgorithm(TransformFieldAlgorithm):
     """
     This algorithm takes a vector layer and normalizes the values of one of
     its fields in the interval 0-1 (or 1-0 if 'inverted' is checked).
     """
-
-    # Constants used to refer to parameters and outputs. They will be
-    # used when calling the algorithm from another algorithm, or when
-    # calling from the QGIS console.
-
-    INPUT = 'INPUT'
-    FIELD_TO_NORMALIZE = 'FIELD_TO_NORMALIZE'
-    INVERTED = 'INVERTED'
-    OUTPUT = 'OUTPUT'
-
-    def tr(self, string):
-        """
-        Returns a translatable string with the self.tr() function.
-        """
-        return QCoreApplication.translate('Processing', string)
-
-    def createInstance(self):
-        return NormalizeFieldAlgorithm()
 
     def name(self):
         """
@@ -69,23 +56,6 @@ class NormalizeFieldAlgorithm(QgsProcessingAlgorithm):
         return self.tr(
             "Min-max normalization (range 0-1) of a vector layer field")
 
-    def group(self):
-        """
-        Returns the name of the group this algorithm belongs to. This string
-        should be localised.
-        """
-        return self.tr('Data Transformation')
-
-    def groupId(self):
-        """
-        Returns the unique ID of the group this algorithm belongs to. This
-        string should be fixed for the algorithm, and must not be localised.
-        The group id should be unique within each provider. Group id should
-        contain lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
-        return 'transform'
-
     def shortHelpString(self):
         """
         Returns a localised short helper string for the algorithm. This string
@@ -94,57 +64,6 @@ class NormalizeFieldAlgorithm(QgsProcessingAlgorithm):
         """
         return self.tr(
             "Min-max normalization (range 0-1) of a vector layer field")
-
-    def icon(self):
-        return QIcon(":/plugins/irmt/transform.svg")
-
-    def svgIconPath(self):
-        return QIcon(":/plugins/irmt/transform.svg")
-
-    def initAlgorithm(self, config=None):
-        """
-        Here we define the inputs and output of the algorithm, along
-        with some other properties.
-        """
-
-        # We add the input vector features source
-        self.addParameter(
-            QgsProcessingParameterFeatureSource(
-                self.INPUT,
-                self.tr('Input layer'),
-                [QgsProcessing.TypeVector]
-            )
-        )
-
-        self.addParameter(
-            QgsProcessingParameterField(
-                self.FIELD_TO_NORMALIZE,
-                description=self.tr("Field to normalize"),
-                defaultValue=None,
-                parentLayerParameterName=self.INPUT,
-                type=QgsProcessingParameterField.Numeric,
-                allowMultiple=False,  # FIXME
-                optional=False,
-            )
-        )
-
-        self.addParameter(
-            QgsProcessingParameterBoolean(
-                self.INVERTED,
-                description=self.tr("Invert transformation"),
-                defaultValue=False,
-            )
-        )
-
-        # We add a feature sink in which to store our processed features (this
-        # usually takes the form of a newly created vector layer when the
-        # algorithm is run in QGIS).
-        self.addParameter(
-            QgsProcessingParameterFeatureSink(
-                self.OUTPUT,
-                self.tr('Output layer')
-            )
-        )
 
     def processAlgorithm(self, parameters, context, feedback):
         """
@@ -249,12 +168,3 @@ class NormalizeFieldAlgorithm(QgsProcessingAlgorithm):
         # dictionary, with keys matching the feature corresponding parameter
         # or output names.
         return {self.OUTPUT: self.dest_id}
-
-    def postProcessAlgorithm(self, context, feedback):
-
-        processed_layer = QgsProcessingUtils.mapLayerFromString(
-            self.dest_id, context)
-
-        # Do smth with the layer, e.g. style it
-
-        return {self.OUTPUT: processed_layer}
---
+++ svir/processing_provider/transform_field.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+# /***************************************************************************
+# Irmt
+#                                 A QGIS plugin
+# OpenQuake Integrated Risk Modelling Toolkit
+#                              -------------------
+#        begin                : 2013-10-24
+#        copyright            : (C) 2013-2019 by GEM Foundation
+#        email                : devops@openquake.org
+# ***************************************************************************/
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterField,
+                       QgsProcessingParameterBoolean,
+                       QgsProcessingParameterFeatureSink)
+
+
+class TransformFieldAlgorithm(QgsProcessingAlgorithm):
+    """
+    FIXME
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    FIELD_TO_NORMALIZE = 'FIELD_TO_NORMALIZE'
+    INVERTED = 'INVERTED'
+    OUTPUT = 'OUTPUT'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return type(self)()
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('Data Transformation')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'transform'
+
+    def icon(self):
+        return QIcon(":/plugins/irmt/transform.svg")
+
+    def svgIconPath(self):
+        return QIcon(":/plugins/irmt/transform.svg")
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        # We add the input vector features source
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT,
+                self.tr('Input layer'),
+                [QgsProcessing.TypeVector]
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterField(
+                self.FIELD_TO_NORMALIZE,
+                description=self.tr("Field to normalize"),
+                defaultValue=None,
+                parentLayerParameterName=self.INPUT,
+                type=QgsProcessingParameterField.Numeric,
+                allowMultiple=False,  # FIXME
+                optional=False,
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.INVERTED,
+                description=self.tr("Invert transformation"),
+                defaultValue=False,
+            )
+        )
+
+        # We add a feature sink in which to store our processed features (this
+        # usually takes the form of a newly created vector layer when the
+        # algorithm is run in QGIS).
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT,
+                self.tr('Output layer')
+            )
+        )
