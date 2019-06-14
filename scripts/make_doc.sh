82f820c4b0e328464418216e257962db6047aaf2
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-06-14 11:16:16 +0200

    Set DEBIAN_FRONTEND=noninteracter in docker exec

--- scripts/make_doc.sh
+++ scripts/make_doc.sh
@@ -8,6 +8,6 @@ docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
  -e DISPLAY=:99 \
  qgis/qgis:latest
 
-docker exec -it qgis sh -c "apt update; apt install -y latexmk texlive-latex-extra python3-matplotlib python3-sphinx python3-sphinx-rtd-theme dvipng"
+docker exec -it qgis sh -c "DEBIAN_FRONTEND=noninteractive; apt update; apt install -y latexmk texlive-latex-extra python3-matplotlib python3-sphinx python3-sphinx-rtd-theme dvipng"
 
 docker exec -it qgis sh -c "export PYTHONPATH=$PYTHONPATH:/oq-irmt-qgis; cd /oq-irmt-qgis/svir; make doc"
--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -8,6 +8,6 @@ docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
  -e DISPLAY=:99 \
  qgis/qgis:latest
 
-docker exec -it qgis sh -c "apt update; apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit python3-nose"
+docker exec -it qgis sh -c "DEBIAN_FRONTEND=noninteractive; apt update; apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit python3-nose"
 
 docker exec -it qgis sh -c "export PYTHONPATH=/usr/share/qgis/python/plugins/:$PYTHONPATH; nosetests3 -v /tests_directory/svir/test/unit/"
