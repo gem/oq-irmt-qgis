f57bbf0a4f38b1fdb1bc7253fb796775d3d82310
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-07-01 15:14:02 +0200

    Style only csv-based input layers that contains the field 'intensity'

--- svir/dialogs/load_inputs_dialog.py
+++ svir/dialogs/load_inputs_dialog.py
@@ -105,9 +105,10 @@ class LoadInputsDialog(QDialog):
             log_msg(str(exc), level='C', message_bar=self.iface.messageBar(),
                     exception=exc)
             raise exc
-        LoadOutputAsLayerDialog.style_maps(
-            self.layer, 'intensity', self.iface, 'input',
-            render_higher_on_top=self.higher_on_top_chk.isChecked())
+        if 'intensity' in [field.name() for field in self.layer.fields()]:
+            LoadOutputAsLayerDialog.style_maps(
+                self.layer, 'intensity', self.iface, 'input',
+                render_higher_on_top=self.higher_on_top_chk.isChecked())
         log_msg('Layer %s was loaded successfully' % layer_name,
                 level='S', message_bar=self.iface.messageBar())
 
