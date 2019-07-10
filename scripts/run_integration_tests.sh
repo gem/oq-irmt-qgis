52ec25f5d68ce515f490531738d88ec8ad569b54
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-07-10 15:06:55 +0200

    Set env variable BRANCH

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
+if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "" ]; then
+    BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+else
+    BRANCH=$TRAVIS_BRANCH
+fi
+export BRANCH
 docker rm -f qgis || true
 docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
  -v `pwd`/../.:/tests_directory \
  -e DISPLAY=:99 \
  -e OQ_ENGINE_HOST='http://172.17.0.1:8800' \
- -e BRANCH='master' \
+ -e BRANCH="$BRANCH" \
  qgis/qgis:latest
 
 docker exec -it qgis sh -c "apt update; DEBIAN_FRONTEND=noninteractive apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit"
 
 # FIXME: let the branch be parametric
-docker exec -it qgis sh -c "git clone -q -b ${BRANCH} --depth=1 https://github.com/gem/oq-engine.git && echo 'Running against oq-engine/${BRANCH}'"
+docker exec -it qgis sh -c "git clone -q -b $BRANCH --depth=1 https://github.com/gem/oq-engine.git && echo 'Running against oq-engine/$BRANCH'"
 # docker exec -it qgis sh -c "git clone -q -b master --depth=1 https://github.com/gem/oq-engine.git && echo 'Running against oq-engine/master'"
 
 docker exec -it qgis sh -c "qgis_setup.sh svir"
