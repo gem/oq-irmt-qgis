da0254e62124c2718e993065336eb7eea507f62a
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-07-10 14:15:55 +0200

    Set env variable BRANCH; still use master always; increment timeout to 180

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -7,13 +7,14 @@ docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
  -v `pwd`/../.:/tests_directory \
  -e DISPLAY=:99 \
  -e OQ_ENGINE_HOST='http://172.17.0.1:8800' \
+ -e BRANCH='master' \
  qgis/qgis:latest
 
 docker exec -it qgis sh -c "apt update; DEBIAN_FRONTEND=noninteractive apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit"
 
-# FIXME: use a specific branch on the oq-engine
-# docker exec -it qgis sh -c "git clone -q -b ${BRANCH} --depth=1 https://github.com/gem/oq-engine.git && echo "Running against oq-engine/${BRANCH}"
-docker exec -it qgis sh -c "git clone -q -b master --depth=1 https://github.com/gem/oq-engine.git"
+# FIXME: let the branch be parametric
+# docker exec -it qgis sh -c "git clone -q -b ${BRANCH} --depth=1 https://github.com/gem/oq-engine.git && echo 'Running against oq-engine/${BRANCH}'"
+docker exec -it qgis sh -c "git clone -q -b master --depth=1 https://github.com/gem/oq-engine.git && echo 'Running against oq-engine/master'"
 
 docker exec -it qgis sh -c "qgis_setup.sh svir"
 
