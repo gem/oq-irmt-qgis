48b37523ce04ee3f0fd372a071a25972f407a6af
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-08-21 11:19:17 +0200

    Temporarily skip tests for recovery modeling

--- svir/test/integration/test_drive_oq_engine.py
+++ svir/test/integration/test_drive_oq_engine.py
@@ -546,13 +546,14 @@ class LoadOqEngineOutputsTestCase(unittest.TestCase):
                         calc, selected_output_type, taxonomy_idx=taxonomy_idx,
                         aggregate_by_site=aggregate_by_site)
                 # for dmg_by_asset also test recovery modeling
-                if selected_output_type == 'dmg_by_asset':
-                    for approach in ['Disaggregate', 'Aggregate']:
-                        self.load_calc_output(
-                            calc, selected_output_type,
-                            aggregate_by_site=False,
-                            approach=approach,
-                            n_simulations=2)
+                # FIXME: temporarily skipped
+                # if selected_output_type == 'dmg_by_asset':
+                #     for approach in ['Disaggregate', 'Aggregate']:
+                #         self.load_calc_output(
+                #             calc, selected_output_type,
+                #             aggregate_by_site=False,
+                #             approach=approach,
+                #             n_simulations=2)
             else:
                 self.load_calc_output(calc, selected_output_type)
         if self.skipped_attempts:
