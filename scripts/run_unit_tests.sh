801ff8f4a704117190724ac0a36ce6c1cd944f42
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-10-24 10:54:23 +0200

    Install and use pytest in travis instead of nose

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -9,7 +9,7 @@ docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
  -e GEM_QGIS_TEST=y \
  qgis/qgis:final-3_8_3
 
-docker exec -it qgis sh -c "apt update; DEBIAN_FRONTEND=noninteractive apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit python3-nose"
+docker exec -it qgis sh -c "apt update; DEBIAN_FRONTEND=noninteractive apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit python3-pytest"
 
 # OGR_SQLITE_JOURNAL=delete prevents QGIS from using WAL, which modifies geopackages even if they are just read
-docker exec -it qgis sh -c "export PYTHONPATH=/usr/share/qgis/python/plugins/:$PYTHONPATH; OGR_SQLITE_JOURNAL=delete nosetests3 -v /tests_directory/svir/test/unit/"
+docker exec -it qgis sh -c "export PYTHONPATH=/usr/share/qgis/python/plugins/:$PYTHONPATH; OGR_SQLITE_JOURNAL=delete python3 -m pytest -v /tests_directory/svir/test/unit/"
