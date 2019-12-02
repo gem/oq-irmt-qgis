f0bcd3208c8723245dc147779bfbf0d51ea333fb
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-12-02 10:14:45 +0100

    For gmf_data admit the possibility that no data is found for given eid

--- svir/test/integration/test_drive_oq_engine.py
+++ svir/test/integration/test_drive_oq_engine.py
@@ -320,12 +320,15 @@ class LoadOqEngineOutputsTestCase(unittest.TestCase):
             output_dict['loading_time'] = loading_time
             self.time_consuming_outputs.append(output_dict)
             self.global_time_consuming_outputs.append(output_dict)
-        if output_dict['output_type'] in OQ_EXTRACT_TO_LAYER_TYPES:
+        output_type = output_dict['output_type']
+        if output_type in OQ_EXTRACT_TO_LAYER_TYPES:
             loaded_layer = self.irmt.iface.activeLayer()
-            self.assertIsNotNone(loaded_layer, 'No layer was loaded')
-            num_feats = loaded_layer.featureCount()
-            self.assertGreater(
-                num_feats, 0, 'The loaded layer does not contain any feature!')
+            if output_type != 'gmf_data':  # it could correctly produce no data
+                self.assertIsNotNone(loaded_layer, 'No layer was loaded')
+                num_feats = loaded_layer.featureCount()
+                self.assertGreater(
+                    num_feats, 0,
+                    'The loaded layer does not contain any feature!')
 
     def load_calc_output(
             self, calc, selected_output_type,
