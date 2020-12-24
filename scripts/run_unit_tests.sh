e7f8db80701b639df612c6ac5102ed0b88109853
Antonio Ettorre <vot4anto@gmail.com>
2020-12-24 11:59:12 +0100

    create test directory inside home and not /

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-#docker rm -f qgis || true
+docker rm -f qgis || true
 docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
  -v `pwd`/../.:/tests_directory \
  -e DISPLAY=:99 \
  -e GEM_QGIS_TEST=y \
  qgis/qgis:final-3_8_3
 
-docker exec qgis bash -c "apt update --allow-releaseinfo-change; DEBIAN_FRONTEND=noninteractive apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit"
-docker exec qgis bash -c "python3 -m pip install pytest"
+docker exec -it qgis bash -c "apt update --allow-releaseinfo-change; DEBIAN_FRONTEND=noninteractive apt install -y python3-scipy python3-matplotlib python3-pyqt5.qtwebkit"
+docker exec -it qgis bash -c "python3 -m pip install pytest"
 
 # OGR_SQLITE_JOURNAL=delete prevents QGIS from using WAL, which modifies geopackages even if they are just read
-docker exec qgis bash -c "export PYTHONPATH=/usr/share/qgis/python/plugins/:$PYTHONPATH; OGR_SQLITE_JOURNAL=delete pytest -v /tests_directory/svir/test/unit/"
+docker exec -it qgis bash -c "export PYTHONPATH=/usr/share/qgis/python/plugins/:$PYTHONPATH; OGR_SQLITE_JOURNAL=delete pytest -v /tests_directory/svir/test/unit/"
