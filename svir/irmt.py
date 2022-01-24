213f935b52da5ecdf9afac1f08a4d599587628f4
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2022-01-24 12:02:49 +0100

    Change a comment just to check if GH actions run

--- svir/irmt.py
+++ svir/irmt.py
@@ -22,7 +22,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-import qgis  # NOQA: it loads the environment
+import qgis  # NOQA: it loads the environment!
 
 import os
 import tempfile
