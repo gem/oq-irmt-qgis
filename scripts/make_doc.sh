f2f5fdc885eb0841ae4d9f8ee209dcf8ce27e248
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2022-12-30 15:22:09 +0100

    Cleanup and additional notes

--- scripts/make_doc.sh
+++ scripts/make_doc.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/usr/bin/docker rm -f qgis || true
+docker rm -f qgis || true
 docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
  -v `pwd`/../.:/oq-irmt-qgis \
  -e DISPLAY=:99 \
@@ -10,5 +10,4 @@ docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
 
 docker exec -it qgis sh -c "apt update --allow-releaseinfo-change; DEBIAN_FRONTEND=noninteractive apt install -y latexmk texlive-latex-extra python3-matplotlib python3-sphinx python3-sphinx-rtd-theme dvipng"
 
-# docker exec -it qgis sh -c "export PYTHONPATH=$PYTHONPATH:/oq-irmt-qgis; cd /oq-irmt-qgis/svir; make doc"
 docker exec -it qgis sh -c "export PYTHONPATH=$PYTHONPATH:/oq-irmt-qgis; cd /oq-irmt-qgis/svir/help; make latexpdf; make html"
