05126d52f32da5cc96da7d27c9ab7d9797e56e05
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2024-05-10 10:33:42 +0200

    Fix make_doc script to produce the documentation in a single call

--- scripts/make_doc.sh
+++ scripts/make_doc.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-docker rm -f qgis || true
-docker run -d --name qgis -v /tmp/.X11-unix:/tmp/.X11-unix \
- -v `pwd`/../.:/oq-irmt-qgis \
- -e DISPLAY=:99 \
- qgis/qgis:release-3_34
-
-docker exec -it qgis sh -c "apt update --allow-releaseinfo-change; DEBIAN_FRONTEND=noninteractive apt install -y latexmk texlive-latex-extra python3-matplotlib python3-sphinx python3-sphinx-rtd-theme dvipng"
-
-docker exec -it qgis sh -c "export PYTHONPATH=$PYTHONPATH:/oq-irmt-qgis; cd /oq-irmt-qgis/svir/help; make latexpdf; make html"
+docker run --rm -d -t --name qgis-ltr-make-doc -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}/../.:/oq-irmt-qgis qgis/qgis:ltr bash -c "apt update --allow-releaseinfo-change; \
+    DEBIAN_FRONTEND=noninteractive apt install -y latexmk texlive-latex-extra python3-matplotlib python3-sphinx python3-sphinx-rtd-theme dvipng; \
+    export DISPLAY=:1.0; \
+    export PYTHONPATH=$PYTHONPATH:/oq-irmt-qgis; \
+    cd /oq-irmt-qgis/svir/help; \
+    make latexpdf; \
+    make html"
+docker logs --follow qgis-ltr-make-doc
