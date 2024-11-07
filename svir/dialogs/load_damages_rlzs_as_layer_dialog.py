ec77356ff68745684debb5dd61859c8e363b45ad
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2024-11-07 09:29:46 +0100

    Revert "Update changelog and add a note"
    This reverts commit 2407b0eb998dd390e1199f3c35cd3acdaeb85883.

--- svir/dialogs/load_damages_rlzs_as_layer_dialog.py
+++ svir/dialogs/load_damages_rlzs_as_layer_dialog.py
@@ -133,8 +133,7 @@ class LoadDamagesRlzsAsLayerDialog(LoadOutputAsLayerDialog):
         self.taxonomy_cbx.setEnabled(True)
 
     def on_loss_type_changed(self):
-        # NOTE: in the past, dmg_states were taken from
-        #       self.dataset[loss_type].dtype.names
+        # self.dmg_states = self.dataset.dtype.names
         self.populate_dmg_state_cbx()
 
     def populate_dmg_state_cbx(self):
