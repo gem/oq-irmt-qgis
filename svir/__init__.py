5dfcd7c019048ca6b4ccbb5a185130a60cb123ee
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2025-02-26 11:24:38 +0100

    Fix deprecation error in python3.12

--- svir/__init__.py
+++ svir/__init__.py
@@ -31,10 +31,7 @@ def classFactory(iface):
     return Irmt(iface)
 
 
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 IS_MATPLOTLIB_INSTALLED = None
 try:
