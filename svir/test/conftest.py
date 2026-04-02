bf60e0ece50388329bc6adfec35474049a6bbe22
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-04-02 11:38:37 +0200

    Improve the fixture to better configure qgis environment

--- svir/test/conftest.py
+++ svir/test/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from qgis.PyQt.QtCore import QSettings
+from qgis.PyQt.QtWidgets import QMenu
+
 
 @pytest.fixture(scope="session", autouse=True)
-def set_qgis_locale():
+def configure_qgis_env(qgis_iface):
     QSettings().setValue("locale/userLocale", "en_US")
+    qgis_iface.firstRightStandardMenu.return_value = QMenu()
