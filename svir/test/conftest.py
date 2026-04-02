5130fd257008fe23e8d5f0765a39aa158242553e
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-04-02 14:56:34 +0200

    Wrap newProject so the patch is re-applied after _mock_methods is cleared

--- svir/test/conftest.py
+++ svir/test/conftest.py
@@ -3,7 +3,7 @@ from qgis.PyQt.QtCore import QSettings
 from qgis.PyQt.QtWidgets import QMenu
 
 
-def _patch_iface(qgis_iface):
+def _patch_first_right_standard_menu(qgis_iface):
     menu = QMenu()
     qgis_iface.firstRightStandardMenu.return_value = menu
     qgis_iface._menu_ref = menu
@@ -12,9 +12,14 @@ def _patch_iface(qgis_iface):
 @pytest.fixture(scope="session", autouse=True)
 def configure_qgis_env(qgis_iface):
     QSettings().setValue("locale/userLocale", "en_US")
-    _patch_iface(qgis_iface)
+    _patch_first_right_standard_menu(qgis_iface)
 
+    # Wrap newProject so the patch is re-applied after _mock_methods is cleared
+    original_new_project = qgis_iface.newProject
 
-@pytest.fixture(autouse=True)
-def reset_iface_between_classes(qgis_iface):
-    _patch_iface(qgis_iface)
+    def patched_new_project(*args, **kwargs):
+        result = original_new_project(*args, **kwargs)
+        _patch_first_right_standard_menu(qgis_iface)
+        return result
+
+    qgis_iface.newProject = patched_new_project
