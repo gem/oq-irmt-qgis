0016427b1bdf78c56523b54e12d226da206cda54
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-04-02 11:56:53 +0200

    Keep a reference to the menu in order to avoid it getting garbage collected

--- svir/test/conftest.py
+++ svir/test/conftest.py
@@ -6,4 +6,7 @@ from qgis.PyQt.QtWidgets import QMenu
 @pytest.fixture(scope="session", autouse=True)
 def configure_qgis_env(qgis_iface):
     QSettings().setValue("locale/userLocale", "en_US")
-    qgis_iface.firstRightStandardMenu.return_value = QMenu()
+    menu = QMenu()
+    qgis_iface.firstRightStandardMenu.return_value = menu
+    # Keep a reference so it doesn't get garbage collected
+    qgis_iface._menu_ref = menu
